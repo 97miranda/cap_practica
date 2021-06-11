5b6207e18bca9ef04c5d2c08fed3a7a5a5d5641c
Jordi <jordilele@gmail.com>
2021-06-11 13:24:27 +0200

    revisar

--- cuda/nn-main.cu
+++ cuda/nn-main.cu
@@ -88,9 +88,10 @@ __global__ void Kernel_SumH(int d_p, char* d_tSet, float* d_WeightIH, float* d_H
 	}
 }
 
-__global__ void Kernel_SumO(int d_p, float *d_Hidden, float *d_WeightHO, float *d_Target, float *d_DeltaO)
+__global__ void Kernel_SumO(int d_p, float *d_Hidden, float *d_WeightHO, float *d_Output, /*float *d_BError,*/ float temp_BError, float *d_Target, float *d_DeltaO)
 {
-	__shared__ float d_SumO[NUMHID], d_Output[NUMOUT], d_BError[NUMOUT];
+	__shared__ float d_SumO[NUMHID];
+	__shared__ float d_BError[NUMOUT];
 
 	unsigned int d_j = threadIdx.x;
         unsigned int d_k = blockIdx.x;
@@ -119,7 +120,8 @@ __global__ void Kernel_SumO(int d_p, float *d_Hidden, float *d_WeightHO, float *
         {
                 __syncthreads();
                 if (d_k < stride)
-                        d_BError[d_k] += d_BError[d_k + stride];
+//                        d_BError[d_k] += d_BError[d_k + stride];
+			temp_BError += d_BError[d_k + stride];
         }
 
 	if (d_j == 0){
@@ -128,6 +130,80 @@ __global__ void Kernel_SumO(int d_p, float *d_Hidden, float *d_WeightHO, float *
         }
 }
 
+__global__ void Kernel_SumDOW(float *d_WeightHO, float *d_DeltaO, float *d_DeltaH, float *d_Hidden)
+{
+	__shared__ float d_SumDOW[NUMOUT];
+
+	unsigned int d_k = threadIdx.x;
+	unsigned int d_j = blockIdx.x;
+	unsigned int d_bsize = blockDim.x;
+
+	d_SumDOW[d_k] = 0.0;
+
+	int pos_Weight = (d_k * d_bsize) + d_j;
+	d_SumDOW[d_k] = d_WeightHO[pos_Weight] * d_DeltaO[d_k];
+
+	for (unsigned int stride = d_bsize/2; stride >= 1; stride >>=1)
+        {
+                __syncthreads();
+                if (d_k < stride)
+                        d_SumDOW[d_k] += d_SumDOW[d_k + stride];
+        }
+
+	if (d_k == 0)
+	{
+		__syncthreads();
+		d_DeltaH[d_j] = d_SumDOW[0] * d_Hidden[d_j] * (1.0 - d_Hidden[d_j]);
+	}
+}
+
+__global__ void Kernel_DeltaIH(int d_p, char *d_tSet, float *d_DeltaWeightIH, float *d_DeltaH, int d_eta, int d_alpha)
+{
+	unsigned int d_i = threadIdx.x;
+        unsigned int d_j = blockIdx.x;
+        unsigned int d_bsize = blockDim.x;
+
+	int pos_Delta = (d_j * d_bsize) + d_i;
+	int pos_tSet = (d_p * d_bsize) + d_i;
+
+	__syncthreads();
+	d_DeltaWeightIH[pos_Delta] = d_eta * d_tSet[pos_tSet] * d_DeltaH[d_j] + d_alpha * d_DeltaWeightIH[pos_Delta];
+}
+
+__global__ void Kernel_DeltaHO(float *d_DeltaWeightHO, float *d_DeltaO, float *d_Hidden, int d_eta, int d_alpha)
+{
+	unsigned int d_j = threadIdx.x;
+        unsigned int d_k = blockIdx.x;
+        unsigned int d_bsize = blockDim.x;
+
+	int pos_DeltaHO = (d_k * d_bsize) + d_j;
+
+	d_DeltaWeightHO[pos_DeltaHO] = d_eta * d_Hidden[d_j] * d_DeltaO[d_k] + d_alpha * d_DeltaWeightHO[pos_DeltaHO];
+}
+
+__global__ void Kernel_WeightIH(float *d_WeightIH, float *d_DeltaWeightIH)
+{
+	unsigned int d_j = threadIdx.x;
+        unsigned int d_i = blockIdx.x;
+        unsigned int d_bsize = blockDim.x;
+
+        int pos_Weight = (d_j * d_bsize) + d_i;
+
+	d_WeightIH[pos_Weight] += d_DeltaWeightIH[pos_Weight];
+}
+
+__global__ void Kernel_WeightHO(float *d_WeightHO, float *d_DeltaWeightHO)
+{
+	unsigned int d_j = threadIdx.x;
+        unsigned int d_k = blockIdx.x;
+        unsigned int d_bsize = blockDim.x;
+
+        int pos_Weight = (d_k * d_bsize) + d_j;
+
+	d_WeightHO[pos_Weight] += d_DeltaWeightHO[pos_Weight];
+}
+
+
 void trainN(){
 	char **tSet;
 
@@ -135,30 +211,48 @@ void trainN(){
 	float Error, BError, eta = 0.3, alpha = 0.5, smallwt = 0.22;
 	int ranpat[NUMPAT];
  	float Hidden[NUMHID], Output[NUMOUT], DeltaO[NUMOUT], DeltaH[NUMHID];
- 	float /*SumO, SumH,*/ SumDOW;
+	float SumO, SumDOW;//SumH;
 
+	//variables bucle 1
+	char *d_tSet;
+	float *d_WeightIH, *d_Hidden;
 	int size_tSet = NUMPAT * 1024 * sizeof(char);
 	int size_WeightIH =  NUMHID * NUMIN * sizeof(float);
-	int size_WeightHO = NUMOUT * NUMHID * sizeof(float);
 	int size_Hidden = NUMHID * sizeof(float);
-	int size_Target = NUMPAT * NUMOUT * sizeof(float);
-	int size_DeltaO = NUMOUT * sizeof(float);
-	char* d_tSet;
-	float *d_WeightIH, *d_WeightHO, *d_Hidden, *d_DeltaO, *d_Target;
-
 	cudaMalloc((void **) &d_tSet, size_tSet);
 	cudaMalloc((void **) &d_WeightIH, size_WeightIH);
 	cudaMalloc((void **) &d_Hidden, size_Hidden);
-	cudaMalloc((void **) &d_Target, size_Target);
+
+	//variables bucle 2
+	float *d_WeightHO, *d_Output, *d_Target, *d_DeltaO;
+//	float *d_BError;
+	float temp_BError;
+	int size_WeightHO = NUMOUT * NUMHID * sizeof(float);
+	int size_Output = NUMOUT * sizeof(float);
+	int size_Target = NUMPAT * NUMOUT * sizeof(float);
+	int size_DeltaO = NUMOUT * sizeof(float);
+//	int size_BError = NUMOUT * sizeof(float);
 	cudaMalloc((void **) &d_WeightHO, size_WeightHO);
+	cudaMalloc((void **) &d_Output, size_Output);
+	cudaMalloc((void **) &d_Target, size_Target);
 	cudaMalloc((void **) &d_DeltaO, size_DeltaO);
+//	cudaMalloc((void **) &d_BError, size_BError);
+	cudaMemcpy(d_Target, Target, size_Target, cudaMemcpyHostToDevice);
+
+	//variables
+	float *d_DeltaH, *d_DeltaWeightIH, *d_DeltaWeightHO;
+	int size_DeltaH = NUMHID * sizeof(float);
+	int size_DeltaWeightIH = NUMHID * NUMIN * sizeof(float);
+	int size_DeltaWeightHO = NUMOUT * NUMHID * sizeof(float);
+	cudaMalloc((void **) &d_DeltaH, size_DeltaH);
+	cudaMalloc((void **) &d_DeltaWeightIH, size_DeltaWeightIH);
+	cudaMalloc((void **) &d_DeltaWeightHO, size_DeltaWeightHO);
 
 //	for (int i = 0; i < NUMPAT; i++){
 //		int pos_tSet = i * 1024;
 //		cudaMemcpy(d_tSet, tSet, size_tSet, cudaMemcpyHostToDevice);
 //		cudaMemcpy2D(d_tSet, pos_tSet, tSet, pos_tSet, 1024, NUMPAT, cudaMemcpyHostToDevice);
 //	}
-	cudaMemcpy(d_Target, Target, size_Target, cudaMemcpyHostToDevice);
 
 	if( (tSet = loadPatternSet( NUMPAT, "optdigits.tra", 1 ) ) == NULL){
        		printf( "Loading Patterns: Error!!\n" );
@@ -205,12 +299,6 @@ void trainN(){
         	       			Hidden[j] = 1.0/(1.0 + exp( -SumH )) ;
         	       		}*/
 
-//				int size_tSet = NUMPAT * 1024 * sizeof(char);
-
-//				char* d_tSet;
-//				float* d_WeightIH;
-//				float* d_Hidden_out;
-
 //				cudaMalloc((char **) &d_tSet, size_tSet);
 //				cudaMalloc((float **) &d_WeightIH, NUMHID * NUMIN * sizeof(float));
 //				cudaMalloc((float **) &d_Hidden_out, NUMHID * sizeof(float));
@@ -222,11 +310,7 @@ void trainN(){
 
 				cudaMemcpy(Hidden, d_Hidden, size_Hidden /*NUMHID*/, cudaMemcpyDeviceToHost);
 
-//				cudaFree(d_tSet);
-//				cudaFree(d_WeightIH);
-//				cudaFree(d_Hidden_out);
-
-/*        	 	      	for(int k = 0 ; k < NUMOUT ; k++ ) {    // compute output unit activations and errors
+        	 	      	for(int k = 0 ; k < NUMOUT ; k++ ) {    // compute output unit activations and errors
        					SumO = 0.0;	
        					for( int j = 0 ; j < NUMHID ; j++ ) 
 						SumO += Hidden[j] * WeightHO[k][j] ;
@@ -234,14 +318,20 @@ void trainN(){
        					BError += 0.5 * (Target[p][k] - Output[k]) * (Target[p][k] - Output[k]) ;   // SSE
        					DeltaO[k] = (Target[p][k] - Output[k]) * Output[k] * (1.0 - Output[k]) ;   // Sigmoidal Outputs, SSE
        	       			}	
-*/
-				cudaMemcpy(d_WeightHO, WeightHO, size_WeightHO , cudaMemcpyHostToDevice);
+
+/*				cudaMemcpy(d_WeightHO, WeightHO, size_WeightHO , cudaMemcpyHostToDevice);
 				cudaMemcpy(d_Hidden, Hidden, size_Hidden, cudaMemcpyHostToDevice);
+//				cudaMemcpy(d_BError, BError, size_BError, cudaMemcpyHostToDevice);
 
-				Kernel_SumO<<< NUMOUT, NUMHID >>>(p, d_Hidden, d_WeightHO, d_Target, d_DeltaO);
+				printf("antes de ir a GPU");
+				Kernel_SumO<<< NUMOUT, NUMHID >>>(p, d_Hidden, d_WeightHO, d_Output,*/ /*d_BError*//*temp_BError, d_Target, d_DeltaO);
+				printf("vuelto de la GPU");
 
+				int size_BError = sizeof(BError);
+				cudaMemcpy(Output, d_Output, size_Output, cudaMemcpyDeviceToHost);
+				cudaMemcpy(BError, temp_BError, size_BError, cudaMemcpyDeviceToHost);
 				cudaMemcpy(DeltaO, d_DeltaO, size_DeltaO, cudaMemcpyDeviceToHost);
-				
+*/				
 	        	       	for(int  j = 0 ; j < NUMHID ; j++ ) {     // update delta weights DeltaWeightIH
         		       		SumDOW = 0.0 ;	
         		       	       	for(int k = 0 ; k < NUMOUT ; k++ ) 
@@ -251,9 +341,31 @@ void trainN(){
        						DeltaWeightIH[j][i] = eta * tSet[p][i] * DeltaH[j] + alpha * DeltaWeightIH[j][i];
        				}
 
-               			for( int k = 0 ; k < NUMOUT ; k ++ )    // update delta weights DeltaWeightHO
+
+/*				cudaMemcpy(d_WeightHO, WeightHO, size_WeightHO, cudaMemcpyHostToDevice);
+				cudaMemcpy(d_DeltaO, DeltaO, size_DeltaO, cudaMemcpyHostToDevice);
+//				cudaMemcpy(d_Hidden, Hidden, size_Hidden, cudaMemcpyHostToDevice);
+
+				Kernel_SumDOW<<< NUMHID, NUMOUT >>>(d_WeightHO, d_DeltaO, d_DeltaH, d_Hidden);
+
+				cudaMemcpy(DeltaH, d_DeltaH, size_DeltaH, cudaMemcpyDeviceToHost);
+//				cudaMemcpy(d_DeltaH, DeltaH, size_DeltaH, cudaMemcpyHostToDevice);
+				cudaMemcpy(d_DeltaWeightIH, DeltaWeightIH, size_DeltaWeightIH, cudaMemcpyHostToDevice);
+
+				Kernel_DeltaIH<<< NUMHID, NUMIN >>>(p, d_tSet, d_DeltaWeightIH, d_DeltaH, eta, alpha);
+
+				cudaMemcpy(DeltaWeightIH, d_DeltaWeightIH, size_DeltaWeightIH, cudaMemcpyDeviceToHost);
+*/
+/*               			for( int k = 0 ; k < NUMOUT ; k ++ )    // update delta weights DeltaWeightHO
                        			for(int  j = 0 ; j < NUMHID ; j++ )
                        				DeltaWeightHO[k][j] = eta * Hidden[j] * DeltaO[k] + alpha * DeltaWeightHO[k][j];
+*/
+				cudaMemcpy(d_DeltaWeightHO, DeltaWeightHO, size_DeltaWeightHO, cudaMemcpyHostToDevice);
+				cudaMemcpy(d_DeltaO, DeltaO, size_DeltaO, cudaMemcpyHostToDevice);
+
+				Kernel_DeltaHO<<< NUMOUT, NUMHID >>>(d_DeltaWeightHO, d_DeltaO, d_Hidden, eta, alpha);
+
+				cudaMemcpy(DeltaWeightHO, d_DeltaWeightHO, size_DeltaWeightHO, cudaMemcpyDeviceToHost);
 			}
 
                		Error += BError;
@@ -265,7 +377,20 @@ void trainN(){
                		for( int k = 0 ; k < NUMOUT ; k ++ )    // update weights WeightHO
                 		for(int j = 0 ; j < NUMHID ; j++ )
                        			WeightHO[k][j] += DeltaWeightHO[k][j] ;
-       		}
+
+/*			cudaMemcpy(d_DeltaWeightIH, DeltaWeightIH, size_DeltaWeightIH, cudaMemcpyHostToDevice);
+                	cudaMemcpy(d_WeightIH, WeightIH, size_WeightIH, cudaMemcpyHostToDevice);
+
+                	Kernel_WeightIH<<< NUMHID, NUMIN >>>(d_WeightIH, d_DeltaWeightIH);
+
+                	cudaMemcpy(WeightIH, d_WeightIH, size_WeightIH, cudaMemcpyDeviceToHost);
+                	cudaMemcpy(d_DeltaWeightHO, DeltaWeightHO, size_DeltaWeightHO, cudaMemcpyHostToDevice);
+                	cudaMemcpy(d_WeightHO, WeightHO, size_WeightHO, cudaMemcpyHostToDevice);
+
+                	Kernel_WeightHO<<< NUMOUT, NUMHID >>>(d_WeightHO, d_DeltaWeightHO);
+
+                	cudaMemcpy(WeightHO, d_WeightHO, size_WeightHO, cudaMemcpyDeviceToHost);
+*/       		}
 
        		Error = Error/((NUMPAT/BSIZE)*BSIZE);	//mean error for the last epoch 		
        		if( !(epoch%100) ) printf( "\nEpoch %-5d :   Error = %f \n", epoch, Error ) ;
@@ -279,9 +404,12 @@ void trainN(){
 	cudaFree(d_tSet);
 	cudaFree(d_WeightIH);
 	cudaFree(d_Hidden);
-	cudaFree(d_Target);
 	cudaFree(d_WeightHO);
+	cudaFree(d_Output);
+	cudaFree(d_Target);
 	cudaFree(d_DeltaO);
+	cudaFree(d_DeltaH);
+//	cudaFree(d_BError);
 
 	printf( "END TRAINING\n" );
 }
